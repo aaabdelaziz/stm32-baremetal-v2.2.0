5ba817336cc3c881fbdb5a9ae0aa3c36c87c33c9
Ahmed Abdelaziz <eng,.>
2026-09-06 22:06:51 +0200

    test: send uart string message

--- 2-UartTx/Src/main.c
+++ 2-UartTx/Src/main.c
@@ -6,7 +6,7 @@ int main(void)
 
     while (1)
     {
-        uart_transmit("A");
+        uart_transmit("sensor_A:10");
 
         // Wait a bit before sending next byte
         for (volatile int i = 0; i < 100000; i++);
